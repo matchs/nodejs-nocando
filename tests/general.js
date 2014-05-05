0be779dcaeb852a810ea1a458715b5e370439410
matchs <contato@mateuschagas.com.br>
2014-05-05 12:36:08 -0300

    Adding first tests structure

---
+++ tests/general.js
@@ -0,0 +1,49 @@
+var assert = require('assert');
+
+describe('Boolean Operators', function(){
+    it('not', function(){
+
+    });
+
+    it('or', function(){
+
+    });
+
+    it('and', function(){
+
+    });
+});
+
+describe('Monads', function(){
+    it('MaybeMonad', function(){
+
+    });
+
+    if('StateMonad', function(){
+
+    });
+});
+
+describe('Registering authorizations', function(){
+    it('saveAuthorization', function(){
+
+    });
+
+    it('can', function(){
+
+    });
+
+    it('revoke', function(){
+
+    });
+
+    it('override', function(){
+
+    });
+});
+
+
+describe('Authorizing', function(){
+  
+
+});
